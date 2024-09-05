233815dccc973ffee52ae9f05d4e290ab314036c
Izabella Mastroianni <izmastroianni@gmail.com>
2024-09-05 09:04:24 -0400

    corrected grep.py program

--- d2-homework/grep.py
+++ d2-homework/grep.py
@@ -3,10 +3,12 @@
 import sys
 
 my_file = open( sys.argv[1] )
+pattern = sys.argv[1]
 
 
 for line in my_file:
     line = line.rstrip("\n")
+    if pattern in line:
     print( line )
   
 my_file.close()
