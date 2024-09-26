bb0ffe238585948a3533e427abcac8006260655f
Izabella Mastroianni <izmastroianni@gmail.com>
2024-09-26 14:57:56 -0400

    fixed multiple errors and completed

--- week2/week2ex2.sh
+++ week2/week2ex2.sh
@@ -11,16 +11,21 @@
 echo -e "MAF\tFeature\tEnrichment" > snp_counts.txt
 
 # loop through each possible MAF file
-snp_files=("chr1_snps_0.*.bed") # maybe change wild card if it doesn't work
-feature_files=("chr1_cCREs.bed" "chr1_exons.bed" "chr1_genes.bed" "other_chr1.bed") 
+snp_files=("chr1_snps_0.1.bed" "chr1_snps_0.2.bed" "chr1_snps_0.3.bed" "chr1_snps_0.4.bed" "chr1_snps_0.5.bed") 
+feature_files=("chr1_cCREs.bed" "chr1_exons2.bed" "chr1_introns.bed" "chr1_other.bed") 
 genome_file=("genome_chr1.bed")
-# per snp file (aka MAF value) per feature file
 
+
+ # make result file
+echo -e "MAF\tFeature\tEnrichment" > snp_counts.txt
+
+
+# per snp file (aka MAF value) per feature file
 for snpfile in "${snp_files[@]}"
     do
         # SNP coverage of the whole chromosome 
         bedtools coverage -a $genome_file -b $snpfile > temp_coverage.txt
-        # sum SNPs from coverage - don't know how to figure out column
+        # sum SNPs from coverage - find column from ppt labels and looked in exon file
         coverage_SNPs_sum=$(awk '{s+=$5}END{print s}' temp_coverage.txt)
         # sum total bases from coverage
         total_bases_sum=$(awk '{s+=$6}END{print s}' temp_coverage.txt)
@@ -31,19 +36,24 @@ for snpfile in "${snp_files[@]}"
                 # SNP coverage of feature file
                 bedtools coverage -a $featurefile -b $snpfile > temp_featuresnp.txt
                 # sum SNPs from coverage
-
+                feature_coverage_SNPs_sum=$(awk '{s+=$5}END{print s}' temp_featuresnp.txt)
                 # sum total bases from coverage
-
+                feature_total_bases_sum=$(awk '{s+=$6}END{print s}' temp_featuresnp.txt)
                 # calculate enrichment
+                ratio=$(echo "$feature_coverage_SNPs_sum / $feature_total_bases_sum" | bc -l)
+                enrichment=$(echo "$ratio / $background" | bc -l)
+                # save to result file
+                echo -e "$snpfile\t$featurefile\t$enrichment" >> snp_counts.txt 
+
 
             done
-
-
 
     done 
 
 
 
+# echo "Coverage SNPs: $feature_coverage_SNPs_sum"
+# echo "Total Bases: $feature_total_bases_sum"
 
 
 
