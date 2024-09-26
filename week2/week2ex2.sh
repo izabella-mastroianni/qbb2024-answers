5cb5fd2274dd74f8544dec59dc3cd55cac4084b3
Izabella Mastroianni <izmastroianni@gmail.com>
2024-09-26 13:44:13 -0400

    progress on 2.1, in inner loop

--- week2/week2ex2.sh
+++ week2/week2ex2.sh
@@ -11,17 +11,33 @@
 echo -e "MAF\tFeature\tEnrichment" > snp_counts.txt
 
 # loop through each possible MAF file
-snp_files=("chr1_snps_0.*.bed") # maybe change wild card
-feature_files=("chr1_cCREs.bed", "chr1_exons.bed", "genome_chr1.bed", "other_chr1.bed") 
+snp_files=("chr1_snps_0.*.bed") # maybe change wild card if it doesn't work
+feature_files=("chr1_cCREs.bed" "chr1_exons.bed" "chr1_genes.bed" "other_chr1.bed") 
+genome_file=("genome_chr1.bed")
 # per snp file (aka MAF value) per feature file
 
-for file in "${snp_files[@]}";
+for snpfile in "${snp_files[@]}"
     do
         # SNP coverage of the whole chromosome 
-        bedtools coverage -a file -b genome_chr1.bed > temp_coverage.txt
+        bedtools coverage -a $genome_file -b $snpfile > temp_coverage.txt
         # sum SNPs from coverage - don't know how to figure out column
-        coverage_SNPs_sum=$(awk '{s+=$1}END{print s}' temp_coverage.txt)
+        coverage_SNPs_sum=$(awk '{s+=$5}END{print s}' temp_coverage.txt)
         # sum total bases from coverage
+        total_bases_sum=$(awk '{s+=$6}END{print s}' temp_coverage.txt)
+        # calculate background,  total # SNPs for given MAF / chromosome length
+        background=$(echo "$coverage_SNPs_sum / $total_bases_sum" | bc -l)
+        for featurefile in "${feature_files[@]}"
+            do
+                # SNP coverage of feature file
+                bedtools coverage -a $featurefile -b $snpfile > temp_featuresnp.txt
+                # sum SNPs from coverage
+
+                # sum total bases from coverage
+
+                # calculate enrichment
+
+            done
+
 
 
     done 
