545abec90a8983fdffe0b3c439783586d028e445
Izabella Mastroianni <izmastroianni@gmail.com>
2024-10-02 13:20:53 -0400

    question 2.1

--- week3/week3_ex1.sh
+++ week3/week3_ex1.sh
@@ -22,4 +22,32 @@ expr 2678192 / 4  # 669548
 # There are 669548 reads in the A01_09.fastq file
 
 
+## Question 1.3 ##
+
+# 
+
+## Question 1.4 ##
+
+
+## Question 1.5 ##
+
+
+
+## Question 2.1 ##
+
+# download sacCer3 reference genome 
+wget https://hgdownload.cse.ucsc.edu/goldenPath/sacCer3/bigZips/sacCer3.fa.gz
+gunzip sacCer3.fa.gz
+
+# create index for sacCer3 reference 
+bwa index sacCer3.fa
+
+# There are chromosomes in the yeast genome 
+
+
+
+
+
+
+
 
