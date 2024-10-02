899b462322835b25c2617d8d06395e3891808785
Izabella Mastroianni <izmastroianni@gmail.com>
2024-10-02 14:06:38 -0400

    finished 2.2 finally got for loop running

--- week3/week3_ex1.sh
+++ week3/week3_ex1.sh
@@ -35,20 +35,33 @@ expr 2678192 / 4  # 669548
 
 ## Question 2.1 ##
 
+# commented out lines below so didn't download and run every time
+
 # download sacCer3 reference genome 
-wget https://hgdownload.cse.ucsc.edu/goldenPath/sacCer3/bigZips/sacCer3.fa.gz
-gunzip sacCer3.fa.gz
+# wget https://hgdownload.cse.ucsc.edu/goldenPath/sacCer3/bigZips/sacCer3.fa.gz
+# gunzip sacCer3.fa.gz
 
 # create index for sacCer3 reference 
-bwa index sacCer3.fa
+# bwa index sacCer3.fa
 
 # find number of chromosome using count of number of times chr appears
-grep -c 'chr' sacCer3.fa
+# grep -c 'chr' sacCer3.fa
+# gives 17, but google says 16, if grep for 'chrXVII' get no result, but get one for 'chrXVI'
+# grep 'chrXVII' sacCer3.fa
 
-# There are 17 chromosomes in the yeast genome 
+# There are 17 chromosomes in the yeast genome but according to google it should be 16 so i'm not sure
 
-## Question 2.1 ##
+## Question 2.2 ##
 
+# for loop to align reads to reference genome 
+for my_sample in *.fastq
+    do 
+        my_sample_basename=`basename ${my_sample} .fastq`
+        read_group="@RG\tID:${my_sample_basename}\tSM:${my_sample_basename}\tPL:illumina"
+        bwa mem -R "${read_group}" sacCer3.fa ${my_sample_basename}.fastq > ${my_sample_basename}.sam
+        samtools sort -@ 4 -O bam -o "${my_sample_basename}.bam" "${my_sample_basename}.sam"
+        samtools index ${my_sample_basename}.bam
+    done
 
 
 
