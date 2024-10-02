d2636fac24002a6f3dc9dc6f55be150debe4c322
Izabella Mastroianni <izmastroianni@gmail.com>
2024-10-02 17:40:02 -0400

    questions 1.3 and 1.4

--- week3/week3_ex1.sh
+++ week3/week3_ex1.sh
@@ -24,10 +24,21 @@ expr 2678192 / 4  # 669548
 
 ## Question 1.3 ##
 
-# 
+# find lines that start with > which are sequences and then count to get size of reference genome 
+grep -v '^>' sacCer3.fa | tr -d '\n' | wc -c
+
+# the S. cerevisiae reference genome is 12157105
+
+# expected average depth of coverage genome length divided by reads
+expr 12157105 / 2678192 #4
+# the depth of coverage is 4
 
 ## Question 1.4 ##
 
+# check file size of fastq files, h is human readable
+du -h *.fastq
+
+# The largest file is A01_62.fastq, 149Mb. The smallest file is A01_27.fastq, 110Mb. 
 
 ## Question 1.5 ##
 
@@ -51,9 +62,8 @@ expr 2678192 / 4  # 669548
 
 # There are 17 chromosomes in the yeast genome but according to google it should be 16 so i'm not sure
 
-## Question 2.2 ##
 
-# for loop to align reads to reference genome 
+# for loop to align reads to reference genome, also includes step 2.4
 for my_sample in *.fastq
     do 
         my_sample_basename=`basename ${my_sample} .fastq`
@@ -62,6 +72,22 @@ for my_sample in *.fastq
         samtools sort -@ 4 -O bam -o "${my_sample_basename}.bam" "${my_sample_basename}.sam"
         samtools index ${my_sample_basename}.bam
     done
+
+## Question 2.2 ##
+
+# to find the number of reads, count number of lines with an @ which correspond to reads
+grep -v '^@' A01_09.sam | wc -l
+
+# In the A01_09 sam file, there are 669548 total read alignments 
+
+## Question 2.3 ##
+
+# count how many lines with an @ and III to specify for chromosome III
+grep -v '^@' A01_09.sam | grep -c '^III'
+
+# In the A01_09 sam file, there are 0 alignments to loci on chromosome III
+
+## Step 2.4 ##
 
 
 
