ada75df7c9760859e24230eb4bb0182dd56eaea5
Izabella Mastroianni <izmastroianni@gmail.com>
2024-10-03 19:09:10 -0400

    separated ex 1 and 2 into two scripts

--- week3/week3_ex1.sh
+++ week3/week3_ex1.sh
@@ -43,57 +43,7 @@ du -h *.fastq
 ## Question 1.5 ##
 FASTQC A01_09.fastq
 
-# The median base quality along the read is around 30. 
+# The median base quality along the read is around 34. 
 # This means that the probabilty a given base called is an error is around 0.1%.
 # I do observe some minimal variation in quality with respect to the position in the read, with slightly worse at the ends and best in the middle. 
-
-
-## Question 2.1 ##
-
-# commented out lines below so didn't download and run every time
-
-# download sacCer3 reference genome 
-# wget https://hgdownload.cse.ucsc.edu/goldenPath/sacCer3/bigZips/sacCer3.fa.gz
-# gunzip sacCer3.fa.gz
-
-# create index for sacCer3 reference 
-# bwa index sacCer3.fa
-
-# find number of chromosome using count of number of times chr appears
-# grep -c 'chr' sacCer3.fa
-# gives 17, but google says 16, if grep for 'chrXVII' get no result, but get one for 'chrXVI'
-# grep 'chrXVII' sacCer3.fa
-
-# There are 17 chromosomes in the yeast genome but according to google it should be 16 so i'm not sure
-
-
-# for loop to align reads to reference genome, also includes step 2.4
-for my_sample in *.fastq
-    do 
-        my_sample_basename=`basename ${my_sample} .fastq`
-        read_group="@RG\tID:${my_sample_basename}\tSM:${my_sample_basename}\tPL:illumina"
-        bwa mem -R "${read_group}" sacCer3.fa ${my_sample_basename}.fastq > ${my_sample_basename}.sam
-        samtools sort -@ 4 -O bam -o "${my_sample_basename}.bam" "${my_sample_basename}.sam"
-        samtools index ${my_sample_basename}.bam
-    done
-
-## Question 2.2 ##
-
-# to find the number of reads, count number of lines with an @ which correspond to reads
-grep -v '^@' A01_09.sam | wc -l
-
-# In the A01_09 sam file, there are 669548 total read alignments 
-
-## Question 2.3 ##
-
-# count how many lines with an @ and III to specify for chromosome III
-grep -v '^@' A01_09.sam | grep -c '^III'
-
-# In the A01_09 sam file, there are 0 alignments to loci on chromosome III
-
-## Step 2.4 ##
-
-
-
-
 
