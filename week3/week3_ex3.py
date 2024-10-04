2a489c78cbf45a8d89baeaa2a550d00dc25e6c9d
Izabella Mastroianni <izmastroianni@gmail.com>
2024-10-03 21:23:10 -0400

    maybe completed ex3

--- week3/week3_ex3.py
+++ week3/week3_ex3.py
@@ -5,18 +5,18 @@ import numpy
 
 ## Exercise 3 ##
 
-# make a list to ensure allele frequencies don't repeat/only get unique ones 
-allele_frequencies = []
-
-# list for read depth
-read_depth = []
 
 # specify VCF file and output AF.txt file
 vcf_file = open('biallelic.vcf')
 af_output_file = open('AF.txt', 'w')
+dp_output_file = open('DP.txt', 'w')
 
-# add a header to the output file for af
+# add a header to the output file for af, only contains allele frequencies
 af_output_file.write('Allele_Frequency\n')
+
+# add header for depth of reads file, only contains read depths
+dp_output_file.write('Read_Depth\n')
+
 
 
 
@@ -25,27 +25,41 @@ for line in vcf_file:
         # look for header with allele frequency info
         if line.startswith('#INFO'):
             print(line.strip())
+            # skip header lines
         continue
-
+    # split lines into fields
     fields = line.rstrip('\n').split('\t')
+
+# For AF
     # INFO field is the 8th column, python counting starts at 0
     info_field = fields[7]
+    # set af_value to none outside loop
+    af_value = None
 
     # since AF is allele frequency and what we want, find that field and pull out the values
     for entry in info_field.split(';'):
         if entry.startswith('AF='):
             af_value = entry.split('=')[1]
-            # don't want af of 0, so exclude
-            if af_value != '0':
-                if af_value not in allele_frequencies:
-                    allele_frequencies.append(af_value)
+            # stop when it finds an AF
+            break 
+    # when find AF, write it into output file
+    if af_value is not None:
+        af_output_file.write(af_value + '\n')
 
-for af in allele_frequencies:
-    af_output_file.write(f"{af}\n")   
+# For DP
+        dp_value = None
+
+    for sample in fields[9:]:
+        sample_data = sample.split(':')
+        dp_value = sample_data[2]
+        if dp_value is not None: 
+            dp_output_file.write(dp_value + '\n')
+
 
 
 
 # close files
 vcf_file.close()
 af_output_file.close()
+dp_output_file.close()
 
